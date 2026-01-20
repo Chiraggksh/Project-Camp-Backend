288f71289281aeca2a97cf3ef10f8c85dcdebe9f
Chirag Kaushik <kaushikchirag187@gmail.com>
2026-01-20 22:42:07 +0530

    async handler done

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -1,6 +1,6 @@
 const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));  //promise.resolve already resolve krke deta h : it returns a promise alr in fulfilled state and uski value jo hm daalte h vo hoti h
   };
 };
 
