bff04a9c8377876f300f98dd6cc3569f4c8e3239
Chirag <kaushikchirag187@gmail.com>
2026-05-05 15:05:29 +0530

    learnt the basic middlewares

--- src/app.js
+++ src/app.js
@@ -4,17 +4,17 @@ import cors from "cors";
 const app = express();
 
 //some basic middleware configurations setup:
-app.use(express.json({ limit: "16kb" })); // this helps to take json data and parse it as req: direct req.body me content ajaega vrna it will return as undefined
-app.use(express.urlencoded({ extended: true, limit: "16kb" })); //use to accept data from url itself and this helps to send objects in objects nested objects used in form like { user: { name: 'Chirag', age: 21 } }
-app.use(express.static("public"));
+app.use(express.json({ limit: "16kb" })); // Converts incoming JSON data → JavaScript object so u can access it using req.body vrna it will return as undefined
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //use to accept data from url itself like form data ya url encoded data and extended true helps to send objects in objects nested objects used in form like { user: { name: 'Chirag', age: 21 } }
+app.use(express.static("public"));  //helps to serve static files in this
 
 //cors configuration
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || ["http://localhost:5173"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Authorization", "Content-Type"],
+    origin: process.env.CORS_ORIGIN?.split(",") || ["http://localhost:5173"], //means in in frontend se allowed hoga
+    credentials: true, //allows cookies and authentication headers that are needed in login, sessions and jwt in cookies
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], //defined http methods ki sirf yahi yahi use hoskte h
+    allowedHeaders: ["Authorization", "Content-Type"], //authorization are used for jwt tokens and content type are used like json
   }),
 );
 
