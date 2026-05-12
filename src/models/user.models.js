fd615bfb612f6cf4e28ea0f06e6e1a878d8a0ade
Chirag <kaushikchirag187@gmail.com>
2026-05-12 20:37:13 +0530

    mail generation done

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -98,7 +98,7 @@ userSchema.methods.generateRefreshToken= function(){
 userSchema.methods.generateTemporaryToken= function(){
   const unHashedToken= crypto.randomBytes(20).toString("hex");
   const hashedToken=crypto.createHash("sha256").update(unHashedToken).digest("hex");
-  const tokenExpiry= Date.now() + (20*60*1000);
+  const tokenExpiry= Date.now() + (20*60*1000); //20 mins
    
   return {unHashedToken,hashedToken,tokenExpiry};
 }
